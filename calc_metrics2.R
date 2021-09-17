fc549585ad547c53919109d0bb5dc3eb8cabe41e
Phil Novack-Gottshall <pnovack-gottshall@ben.edu>
2021-09-17 16:39:51 -0500

    improved error handling for troublesome cases

--- calc_metrics2.R
+++ calc_metrics2.R
@@ -33,44 +33,48 @@ calc_metrics2 <- function(sample = NA,
   odir <-
     setwd(tempdir())     # Specify the pre-built (and CPU-process unique) temp directory for storage of vert.txt temp files for convex hull calculations
   on.exit(setwd(odir))
-  sam.out <- data.frame(S = integer(1), H = integer(1), D = numeric(1), 
-                        M = numeric(1), V = numeric(1), R = numeric(1), 
-                        FRic = numeric(1), FEve = numeric(1), FDiv = numeric(1), 
-                        FDis = numeric(1), qual.FRic = numeric(1))
+#  sam.out <- data.frame(S = integer(1), H = integer(1), D = numeric(1), M = numeric(1), V = numeric(1), R = numeric(1), FRic = numeric(1), FEve = numeric(1), FDiv = numeric(1), FDis = numeric(1), qual.FRic = numeric(1))
+  sam.out <- data.frame(S = NA, H = NA, D = NA, M = NA, V = NA, R = NA, FRic = NA, FEve = NA, FDiv = NA, FDis = NA, qual.FRic = NA)
   sam <- sample[1:s,]
   sam.out$S <- s
   H <- nrow(unique(dist.sam))
   sam.out$H <- H
-  if (any(is.nan(dist.sam)) | length(dist.sam) == 0)
-    next
-  sam.out$D <- mean(dist.sam, na.rm = TRUE)
-  sam.out$M <- max(dist.sam, na.rm = TRUE)
-  sam.out$V <- sqrt(sum(apply(sam, 2, var, na.rm = TRUE), na.rm = TRUE))
-
-  # Get PCoA components (with switch for whether corrected or not)
-  if (pcoa$correction[1] == "none") {
-    vectors <- pcoa$vectors
-    eigenvalues <- pcoa$values$Eigenvalues
-  } else {
-    vectors <- pcoa$vectors.cor
-    eigenvalues <- pcoa$values$Corr.eig
+  if (!any(is.nan(dist.sam)) | length(dist.sam) != 0) {
+    sam.out$D <- mean(dist.sam, na.rm = TRUE)
+    sam.out$M <- max(dist.sam, na.rm = TRUE)
+    sam.out$V <- sqrt(sum(apply(sam, 2, var, na.rm = TRUE), na.rm = TRUE))
+    
+    # Confirm 'pcoa' exists before calculating sum of ranges
+    if (!is.null(pcoa)) {
+  
+      # Get PCoA components (with switch for whether corrected or not)
+      if (pcoa$correction[1] == "none") {
+        vectors <- pcoa$vectors
+        eigenvalues <- pcoa$values$Eigenvalues
+      } else {
+        vectors <- pcoa$vectors.cor
+        eigenvalues <- pcoa$values$Corr.eig
+      }
+      # Standardize the eigenvectors according to magnitude of eigenvalues (if desired)
+      if(stand.pcoa)
+        vectors <- stand.pcoa.fn(vectors = vectors, eigenvalues = eigenvalues)
+    
+      maxes <- apply(vectors, 2, max, na.rm = TRUE)
+      mins <- apply(vectors, 2, min, na.rm = TRUE)
+      sam.out$R <- sum(maxes - mins, na.rm = TRUE)
+    }
+    
+    # Move on to functional diversity metrics
+    if (s > m | H > m) {
+      FD <- simple.dbFD(pcoa = pcoa, dist.matrix = dist.sam, m = m, 
+                        stand.pcoa = stand.pcoa, 
+                        calc.FRic.and.FDiv = calc.FRic.and.FDiv)
+      sam.out$FRic <- FD$FRic
+      sam.out$FEve <- FD$FEve
+      sam.out$FDiv <- FD$FDiv
+      sam.out$FDis <- FD$FDis
+      sam.out$qual.FRic <- FD$qual.FRIC
+    }
   }
-  # Standardize the eigenvectors according to magnitude of eigenvalues (if desired)
-  if(stand.pcoa)
-    vectors <- stand.pcoa.fn(vectors = vectors, eigenvalues = eigenvalues)
-
-  maxes <- apply(vectors, 2, max, na.rm = TRUE)
-  mins <- apply(vectors, 2, min, na.rm = TRUE)
-  sam.out$R <- sum(maxes - mins, na.rm = TRUE)
-  if (s <= m | H <= m)
-    next
-  FD <- simple.dbFD(pcoa = pcoa, dist.matrix = dist.sam, m = m, 
-                    stand.pcoa = stand.pcoa, 
-                    calc.FRic.and.FDiv = calc.FRic.and.FDiv)
-  sam.out$FRic <- FD$FRic
-  sam.out$FEve <- FD$FEve
-  sam.out$FDiv <- FD$FDiv
-  sam.out$FDis <- FD$FDis
-  sam.out$qual.FRic <- FD$qual.FRIC
   return(sam.out)
 }
--- simple.dbFD.R
+++ simple.dbFD.R
@@ -31,136 +31,153 @@ simple.dbFD <- function(pcoa = NULL, dist.matrix = NULL, m = NA,
     return(vectors)
   }
   
-  # Get components (with switch for whether corrected or not)
-  if (pcoa$correction[1] == "none") {
-    vectors <- pcoa$vectors
-    eigenvalues <- pcoa$values$Eigenvalues
-  } else {
-    vectors <- pcoa$vectors.cor
-    eigenvalues <- pcoa$values$Corr.eig
-  }
-  # Standardize the eigenvectors according to magnitude of eigenvalues (if desired)
-  if(stand.pcoa)
-    vectors <- stand.pcoa.fn(vectors = vectors, eigenvalues = eigenvalues)
+  # Set default values (if incalculable) as NA
+  FRic <- FEve <- FDis <- FDiv <- qual.FRic <- NA
+  
   if (!"dist" %in% class(dist.matrix))
     dist.matrix <- as.dist(dist.matrix)
-  if (is.na(m))
-    stop("Must supply 'm' to calculate functional diversity metrics.")
-  # The eigenvectors with 'm' reduced dimensionality:
-  m.vectors <- vectors[, 1:m]
   
-  ## FDis: Functional dispersion
-  # No modification needed. Use dbFD::fdisp(d).
-  #    d = distance matrix (in class dist)
-  FDis <- unname(FD::fdisp(d = dist.matrix)$FDis)
+  # Calculate FDis first, because does not use PCoA vectors
   
-  ## FRic: Functional richness
-  # With earlier processing of reduced dimensionality and standardized PCoA, no
-  # novel function needed.
-  FRic <- NA
-  if (calc.FRic.and.FDiv)
-    FRic <- geometry::convhulln(m.vectors, "FA")$vol
-  
-  ## FDiv: Functional divergence
-  #    v = eigenvector matrix output from ape::pcoa (with 'm' reduced 
-  #        dimensionality and stand.pcoa pre-standardized).
-  calc.FDiv <- function(v = NULL) {
-    # calculate vertices
-    vert0 <- geometry::convhulln(v, "Fx TO 'vert.txt'")
-    vert1 <- scan("vert.txt", quiet = TRUE)
-    vert2 <- vert1 + 1
-    vertices <- vert2[-1]
-    # traits values of vertices
-    trvertices <- v[vertices, ]
-    # coordinates of the center of gravity of the vertices (Gv)
-    baryv <- apply(trvertices, 2, mean)
-    # Euclidean distances to Gv (dB)
-    S <- nrow(v)
-    distbaryv <- rep(0, S)
-    for (j in 1:S) {
-      distbaryv[j] <- (sum((v[j, ] - baryv) ^ 2)) ^ 0.5
-    }
-    # mean of dB values
-    meandB <- mean(distbaryv)
-    # deviations to mean of db
-    devdB <- distbaryv - meandB
-    # computation of FDiv (using equal-weighting, i.e., ignoring rel. abund.)
-    out <- (sum(devdB) + meandB) / (sum(abs(devdB)) + meandB)
-    return(out)
+  # But first confirm the distance matrix is complete
+  if (all(!is.na(dist.matrix))) {
+    
+    ## FDis: Functional dispersion
+    # No modification needed. Use dbFD::fdisp(d).
+    #    d = distance matrix (in class dist)
+    FDis <- unname(FD::fdisp(d = dist.matrix)$FDis)
+    
   }
   
-  FDiv <- NA
-  if (calc.FRic.and.FDiv)
-    FDiv <- calc.FDiv(v = m.vectors)
-
-  ## FEve: Functional evenness
-  #    v = eigenvector matrix output from ape::pcoa (with all 'm' eigenvalues
-  #        and with stand.pcoa pre-standardized).
-  calc.FEve <- function(v = NULL) {
-    # computation of minimum spanning tree and conversion of the 'mst' matrix
-    # into 'dist' class
-    tr.dist <- dist(v)
-    # The next step is very slow (for large data sets)!!!
-    linkmst <- ape::mst(tr.dist)
-    mstvect <- as.dist(linkmst)
-    # computation of the pairwise cumulative relative abundances and conversion
-    # into 'dist' class
-    S <- nrow(v)
-    # Use equal-weighting (ignoring relative abundance)
-    abund2 <- matrix(1, nrow = S, ncol = S)
-    abund2vect <- as.dist(abund2)
-    # computation of EW for the (S - 1) segments to link the S points
-    EW <- rep(0, S - 1)
-    flag <- 1
-    for (m in 1:((S - 1) * S / 2)) {
-      if (mstvect[m] != 0) {
-        EW[flag] <- tr.dist[m] / (abund2vect[m])
-        flag <- flag + 1
+  # Confirm pcoa exists before calculating rest
+  if (!is.null(pcoa)) {
+    
+    # Get components (with switch for whether corrected or not)
+    if (pcoa$correction[1] == "none") {
+      vectors <- pcoa$vectors
+      eigenvalues <- pcoa$values$Eigenvalues
+    } else {
+      vectors <- pcoa$vectors.cor
+      eigenvalues <- pcoa$values$Corr.eig
+    }
+      
+    # Standardize the eigenvectors according to magnitude of eigenvalues (if desired)
+    if(stand.pcoa)
+      vectors <- stand.pcoa.fn(vectors = vectors, eigenvalues = eigenvalues)
+    if (is.na(m))
+      stop("Must supply 'm' to calculate functional diversity metrics.")
+    # The eigenvectors with 'm' reduced dimensionality:
+    m.vectors <- vectors[, 1:m]
+    
+    ## FRic: Functional richness
+    # With earlier processing of reduced dimensionality and standardized PCoA, no
+    # novel function needed.
+    FRic <- NA
+    if (calc.FRic.and.FDiv)
+      FRic <- geometry::convhulln(m.vectors, "FA")$vol
+    
+    ## FDiv: Functional divergence
+    #    v = eigenvector matrix output from ape::pcoa (with 'm' reduced 
+    #        dimensionality and stand.pcoa pre-standardized).
+    calc.FDiv <- function(v = NULL) {
+      # calculate vertices
+      vert0 <- geometry::convhulln(v, "Fx TO 'vert.txt'")
+      vert1 <- scan("vert.txt", quiet = TRUE)
+      vert2 <- vert1 + 1
+      vertices <- vert2[-1]
+      # traits values of vertices
+      trvertices <- v[vertices, ]
+      # coordinates of the center of gravity of the vertices (Gv)
+      baryv <- apply(trvertices, 2, mean)
+      # Euclidean distances to Gv (dB)
+      S <- nrow(v)
+      distbaryv <- rep(0, S)
+      for (j in 1:S) {
+        distbaryv[j] <- (sum((v[j, ] - baryv) ^ 2)) ^ 0.5
       }
+      # mean of dB values
+      meandB <- mean(distbaryv)
+      # deviations to mean of db
+      devdB <- distbaryv - meandB
+      # computation of FDiv (using equal-weighting, i.e., ignoring rel. abund.)
+      out <- (sum(devdB) + meandB) / (sum(abs(devdB)) + meandB)
+      return(out)
     }
-    # computation of the PEW and comparison with 1 / S - 1, finally computation
-    # of FEve
-    minPEW <- rep(0, S - 1)
-    OdSmO <- 1 / (S - 1)
-    for (l in 1:(S - 1)) {
-      minPEW[l] <- min((EW[l] / sum(EW)), OdSmO)
+    
+    FDiv <- NA
+    if (calc.FRic.and.FDiv)
+      FDiv <- calc.FDiv(v = m.vectors)
+  
+    ## FEve: Functional evenness
+    #    v = eigenvector matrix output from ape::pcoa (with all 'm' eigenvalues
+    #        and with stand.pcoa pre-standardized).
+    calc.FEve <- function(v = NULL) {
+      # computation of minimum spanning tree and conversion of the 'mst' matrix
+      # into 'dist' class
+      tr.dist <- dist(v)
+      # The next step is very slow (for large data sets)!!!
+      linkmst <- ape::mst(tr.dist)
+      mstvect <- as.dist(linkmst)
+      # computation of the pairwise cumulative relative abundances and conversion
+      # into 'dist' class
+      S <- nrow(v)
+      # Use equal-weighting (ignoring relative abundance)
+      abund2 <- matrix(1, nrow = S, ncol = S)
+      abund2vect <- as.dist(abund2)
+      # computation of EW for the (S - 1) segments to link the S points
+      EW <- rep(0, S - 1)
+      flag <- 1
+      for (m in 1:((S - 1) * S / 2)) {
+        if (mstvect[m] != 0) {
+          EW[flag] <- tr.dist[m] / (abund2vect[m])
+          flag <- flag + 1
+        }
+      }
+      # computation of the PEW and comparison with 1 / S - 1, finally computation
+      # of FEve
+      minPEW <- rep(0, S - 1)
+      OdSmO <- 1 / (S - 1)
+      for (l in 1:(S - 1)) {
+        minPEW[l] <- min((EW[l] / sum(EW)), OdSmO)
+      }
+      FEve <- ((sum(minPEW)) - OdSmO) / (1 - OdSmO)
+      return(FEve)
     }
-    FEve <- ((sum(minPEW)) - OdSmO) / (1 - OdSmO)
-    return(FEve)
+    
+    FEve <- calc.FEve(v = vectors)
+    
+    ## qual.FRic: Quality of reduced-dimensionality hyperspace used in FRic
+    #    v = eigenvector matrix output from ape::pcoa (with 'm' reduced 
+    #        dimensionality and stand.pcoa pre-standardized).
+    calc.qual.FRic <- function(pcoa = NULL, dist.matrix = NULL, m = NA) {
+  
+      # What 'correction' was used in ape::pcoa to handle negative eigenvalues?
+      corr <- pcoa$correction[1]
+      
+      eigenvalues <- pcoa$values$Corr_eig
+      
+      if (corr != "none")
+        qual.FRic <- sum(eigenvalues[1:m]) / sum(eigenvalues)
+      
+      if (corr == "none") {
+        # compute the eigenvalues (including negative ones)
+        delta <- -0.5 * bicenter.wt(dist.matrix * dist.matrix)
+        lambda <- eigen(delta, symmetric = TRUE, only.values = TRUE)$values
+        # calculate corrected R^2-like ratio
+        sum.m <- sum(lambda[1:m])
+        sum.n <- sum(lambda)
+        lambda.neg <- c(lambda[lambda < 0])
+        max.neg <- abs(min(lambda.neg))
+        qual.FRic <- (sum.m + (length(lambda[1:m]) * max.neg)) /
+          (sum.n + ((length(lambda) - 1) * max.neg))
+      }
+      
+      return(qual.FRic)
+    }
+    
+    qual.FRic <- calc.qual.FRic(pcoa = pcoa, dist.matrix = dist.matrix, m = m)
+    
   }
-  
-  FEve <- calc.FEve(v = vectors)
-  
-  ## qual.FRic: Quality of reduced-dimensionality hyperspace used in FRic
-  #    v = eigenvector matrix output from ape::pcoa (with 'm' reduced 
-  #        dimensionality and stand.pcoa pre-standardized).
-  calc.qual.FRic <- function(pcoa = NULL, dist.matrix = NULL, m = NA) {
-
-    # What 'correction' was used in ape::pcoa to handle negative eigenvalues?
-    corr <- pcoa$correction[1]
-    
-    eigenvalues <- pcoa$values$Corr_eig
-    
-    if (corr != "none")
-      qual.FRic <- sum(eigenvalues[1:m]) / sum(eigenvalues)
-    
-    if (corr == "none") {
-      # compute the eigenvalues (including negative ones)
-      delta <- -0.5 * bicenter.wt(dist.matrix * dist.matrix)
-      lambda <- eigen(delta, symmetric = TRUE, only.values = TRUE)$values
-      # calculate corrected R^2-like ratio
-      sum.m <- sum(lambda[1:m])
-      sum.n <- sum(lambda)
-      lambda.neg <- c(lambda[lambda < 0])
-      max.neg <- abs(min(lambda.neg))
-      qual.FRic <- (sum.m + (length(lambda[1:m]) * max.neg)) /
-        (sum.n + ((length(lambda) - 1) * max.neg))
-    }
-    
-    return(qual.FRic)
-  }
-  
-  qual.FRic <- calc.qual.FRic(pcoa = pcoa, dist.matrix = dist.matrix, m = m)
   
   # Clean up output
   out <- list(FRic = FRic, FEve = FEve, FDis = FDis, FDiv = FDiv, 
