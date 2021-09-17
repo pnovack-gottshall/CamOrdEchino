17c630f6a0eebe603418e1d9ab25f0e878f0f85d
Phil Novack-Gottshall <pnovack-gottshall@ben.edu>
2021-09-17 11:20:53 -0500

    added sum of ranges calculation, plus switch for handing corrected/not PCoA

--- calc_metrics2.R
+++ calc_metrics2.R
@@ -7,6 +7,8 @@
 ## according to magnitude of eigenvalues. (4) Streamlines error-checking to make
 ## faster. Code by Phil Novack-Gottshall and taken from
 ## https://github.com/pnovack-gottshall/ecospace/blob/master/R/calc_metrics.R.
+## (5) Adds sum of total ranges (using option to use Dineen, et al. (2019)
+## standardization of PCoA eigenvectors).
 
 calc_metrics2 <- function(sample = NA,
                           dist.sam = NULL,
@@ -14,6 +16,19 @@ calc_metrics2 <- function(sample = NA,
                           m = 3,
                           stand.pcoa = TRUE,
                           calc.FRic.and.FDiv = TRUE) {
+
+  # Functional to apply Dineen, et al. (2019, Biology Lettters) standardization
+  # of PCoA eigenvectors according to the magnitude of the first eigenvalue.
+  # (Their eq. 1 in Supplementary Information.)
+  stand.pcoa.fn <- function(vectors = NULL, eigenvalues = NULL) {
+    nc <- ncol(vectors)
+    first.e <- eigenvalues[1]
+    for (c in 1:nc) {
+      vectors[, c] <- vectors[, c] * eigenvalues[c] / first.e
+    }
+    return(vectors)
+  }
+  
   s <- nrow(sample)
   odir <-
     setwd(tempdir())     # Specify the pre-built (and CPU-process unique) temp directory for storage of vert.txt temp files for convex hull calculations
@@ -31,8 +46,21 @@ calc_metrics2 <- function(sample = NA,
   sam.out$D <- mean(dist.sam, na.rm = TRUE)
   sam.out$M <- max(dist.sam, na.rm = TRUE)
   sam.out$V <- sqrt(sum(apply(sam, 2, var, na.rm = TRUE), na.rm = TRUE))
-  maxes <- apply(pcoa$vectors, 2, max, na.rm = TRUE)
-  mins <- apply(pcoa$vectors, 2, min, na.rm = TRUE)
+
+  # Get PCoA components (with switch for whether corrected or not)
+  if (pcoa$correction[1] == "none") {
+    vectors <- pcoa$vectors
+    eigenvalues <- pcoa$values$Eigenvalues
+  } else {
+    vectors <- pcoa$vectors.cor
+    eigenvalues <- pcoa$values$Corr.eig
+  }
+  # Standardize the eigenvectors according to magnitude of eigenvalues (if desired)
+  if(stand.pcoa)
+    all.vectors <- stand.pcoa.fn(vectors = vectors, eigenvalues = eigenvalues)
+
+  maxes <- apply(all.vectors, 2, max, na.rm = TRUE)
+  mins <- apply(all.vectors, 2, min, na.rm = TRUE)
   sam.out$R <- sum(maxes - mins, na.rm = TRUE)
   if (s <= m | H <= m)
     next
